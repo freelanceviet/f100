0fac6df713c12aa35ed9c90e4d079a5965b38066
luckyluck4989 <luckyluck4989@gmail.com>
2014-10-27 00:28:50 +0700

    home_page.jade

--- app/server/routes/home_page.js
+++ app/server/routes/home_page.js
@@ -1,20 +1,30 @@
 var Mogodb   = require('../mongodb/connection');
-var AM = require('../modules/home-page-manager');
+var CM = require('../modules/contest-manager');
+var JM = require('../modules/job-manager');
+
 module.exports = function (app) {
 	//--------------------------------------
 	// router admin home page
 	//--------------------------------------
 	app.get('/', function (req, res) {
-		if(req.session.user==null){
-			res.render('block/font-end/home_page', {
-				user : null,
-				title:"Home page"
+		CM.getContestDefault(8, 0, function(errContests, resContests) {
+			JM.getJobDefault(20, 0, function(errJobs, resJobs){
+				if(req.session.user==null){
+					res.render('block/font-end/home_page', {
+						user : null,
+						title:"Home page",
+						resContests : resContests,
+						resJobs : resJobs
+					});
+				}else{
+					res.render('block/font-end/home_page', {
+						user : req.session.user,
+						title : "Home page",
+						resContests : resContests,
+						resJobs : resJobs
+					});
+				}
 			});
-		}else{
-			res.render('block/font-end/home_page', {
-				user : req.session.user,
-				title : "Home page"
-			});
-		}
+		});
 	});
 }
