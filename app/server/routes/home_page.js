ee9fda502272b9560c3fe7f814866caada814339
luckyluck4989 <luckyluck4989@gmail.com>
2014-11-09 10:48:33 +0700

    hourlie home page

--- app/server/routes/home_page.js
+++ app/server/routes/home_page.js
@@ -1,6 +1,7 @@
 var Mogodb   = require('../mongodb/connection');
 var CM = require('../modules/contest-manager');
 var ALL = require('../modules/public-manager');
+var HM = require('../modules/hourlie-manager');
 var JM = require('../modules/job-manager');
 
 module.exports = function (app) {
@@ -11,23 +12,27 @@ module.exports = function (app) {
 		CM.getContestDefault(8, 0, function(errContests, resContests) {
 			JM.getJobDefault(4, 0, function(errJobs, resJobs){
 				ALL.getAllCategories(function(errCategories, resCategories){
-					if(req.session.user==null){
-						res.render('block/font-end/home_page', {
-							user : null,
-							title:"Home page",
-							resCategories : resCategories,
-							resContests : resContests,
-							resJobs : resJobs
-						});
-					}else{
-						res.render('block/font-end/home_page', {
-							user : req.session.user,
-							title : "Home page",
-							resCategories : resCategories,
-							resContests : resContests,
-							resJobs : resJobs
-						});
-					}
+					HM.getHourlieDefault(8, 0, function(errHourlies, resHourlies) {
+						if(req.session.user==null){
+							res.render('block/font-end/home_page', {
+								user : null,
+								title:"Home page",
+								resCategories : resCategories,
+								resContests : resContests,
+								resJobs : resJobs,
+								resHourlies : resHourlies
+							});
+						}else{
+							res.render('block/font-end/home_page', {
+								user : req.session.user,
+								title : "Home page",
+								resCategories : resCategories,
+								resContests : resContests,
+								resJobs : resJobs,
+								resHourlies : resHourlies
+							});
+						}
+					});
 				});
 			});
 		});
