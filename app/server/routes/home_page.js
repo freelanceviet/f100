27de857e3ae79af197c249bdb2728a73e0c7353e
luckyluck4989 <luckyluck4989@gmail.com>
2014-11-09 09:44:28 +0700

    home page set data

--- app/server/routes/home_page.js
+++ app/server/routes/home_page.js
@@ -1,5 +1,6 @@
 var Mogodb   = require('../mongodb/connection');
 var CM = require('../modules/contest-manager');
+var ALL = require('../modules/public-manager');
 var JM = require('../modules/job-manager');
 
 module.exports = function (app) {
@@ -8,22 +9,26 @@ module.exports = function (app) {
 	//--------------------------------------
 	app.get('/', function (req, res) {
 		CM.getContestDefault(8, 0, function(errContests, resContests) {
-			JM.getJobDefault(20, 0, function(errJobs, resJobs){
-				if(req.session.user==null){
-					res.render('block/font-end/home_page', {
-						user : null,
-						title:"Home page",
-						resContests : resContests,
-						resJobs : resJobs
-					});
-				}else{
-					res.render('block/font-end/home_page', {
-						user : req.session.user,
-						title : "Home page",
-						resContests : resContests,
-						resJobs : resJobs
-					});
-				}
+			JM.getJobDefault(4, 0, function(errJobs, resJobs){
+				ALL.getAllCategories(function(errCategories, resCategories){
+					if(req.session.user==null){
+						res.render('block/font-end/home_page', {
+							user : null,
+							title:"Home page",
+							resCategories : resCategories,
+							resContests : resContests,
+							resJobs : resJobs
+						});
+					}else{
+						res.render('block/font-end/home_page', {
+							user : req.session.user,
+							title : "Home page",
+							resCategories : resCategories,
+							resContests : resContests,
+							resJobs : resJobs
+						});
+					}
+				});
 			});
 		});
 	});
